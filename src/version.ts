64f064c01411c0b847b1c1b71a40cc73b5188dd1
Markirov <marcosfenollar@gmail.com>
2026-04-24 16:16:10 +0200

    Subida limpia del proyecto (v2.1.2)

--- src/version.ts
+++ src/version.ts
@@ -8,5 +8,5 @@
 // Importante: no cambies el formato de la linea `export const VERSION = 'x.y.z';`
 // porque el script lo detecta con regex.
 
-export const VERSION = '2.1.1';
+export const VERSION = '2.1.2';
 export const VERSION_DISPLAY = `Ver. ${VERSION}`;
